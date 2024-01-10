b75ed4900443a9ca8207b623e5bdf945498685b6
alinebarbosasilva <alinnee_barbosa@outlook.com>
2024-01-10 03:36:45 -0300

    Aprendendo Java

---
+++ src/defaultPackage/Main.java
@@ -0,0 +1,163 @@
+package defaultPackage;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+
+      String s1, s2, s3;
+      int x;
+
+      x = sc.nextInt();
+      sc.nextLine();
+      s1 = sc.nextLine();
+      s2 = sc.nextLine();
+      s3 = sc.nextLine();
+
+      System.out.println(x);
+      System.out.println(s1);
+      System.out.println(s2);
+      System.out.println(s3);
+
+//      Locale.setDefault(Locale.US);
+//      Scanner sc = new Scanner(System.in);
+//
+//      String x;
+//      int y;
+//      double z;
+//
+//      x = sc.next();
+//      y = sc.nextInt();
+//      z = sc.nextDouble();
+//      System.out.println("Dados digitados: ");
+//
+//      System.out.println(x);
+//      System.out.println(y);
+//      System.out.println(z);
+
+//      String nome;
+//      nome = sc.next();
+//      System.out.println("Você digitou: " + nome);
+//      sc.close();
+
+
+//      int age;
+//      age = sc.nextInt();
+//      System.out.println("Você digitou " + age);
+//
+//      sc.close();
+
+//      double age;
+//      age = sc.nextDouble();
+//      System.out.printf("Você digitou: %.2f", age);
+//      sc.close();
+
+//      char x;
+//      x = sc.next().charAt(0);
+//      System.out.println("Você digitou: " + x);
+//      sc.close();
+
+//
+//      String name;
+//      int age;
+//      double income;
+//
+//      name = sc.next();
+//      age = sc.nextInt();
+//      income = sc.nextDouble();
+//
+//      System.out.println("Dados digitados: ");
+//      System.out.println(name);
+//      System.out.println(age);
+//      System.out.println(income);
+//
+//      sc.close();
+
+    
+
+  }
+
+
+//    public static void main(String[] args) {
+//
+//        int y = 32;
+//        double x = 10.35784;
+//
+//        System.out.println(y);
+//        System.out.printf("%.2f%n", x);
+//        Locale.setDefault(Locale.US);
+//        System.out.printf("%.4f%n", x);
+//        System.out.println(x);
+//        System.out.println("Hello World!");
+//        System.out.print("Hi!");
+//        System.out.println("Good morning!");
+//
+//        System.out.println("Result = " + y + " METROS");
+//        System.out.printf("Result = %.2f metros%n", x);
+//
+//
+//        String name = "Maria";
+//        int age = 31;
+//        double income = 4000.0;
+//        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", name, age, income);
+//
+//    }
+
+
+//    public static void main(String[] args) {
+//        String product1 = "Computer";
+//        String product2 = "Office desk";
+//
+//        byte age = 30;
+//        int code = 5290;
+//        char gender = 'F';
+//
+//        double price1 = 2100.0;
+//        double price2 = 650.50;
+//        double measure = 53.234567;
+//
+//        System.out.println("Products:");
+//        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+//        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+//        System.out.println();
+//        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code,gender);
+//        System.out.println();
+//        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+//        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
+//        Locale.setDefault(Locale.US);
+//        System.out.printf("US decimal point: %.3f",measure);
+//    }
+
+
+//    public static void main(String[] args) {
+//        int x, y;
+//
+//        x = 5;
+//        y = 2 * x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+//        double b, B, h, area;
+//        b = 6.0;
+//        B = 8.0;
+//        h = 5.0;
+//        area = (b + B) / 2.0 * h;
+//        System.out.println(area);
+//    }
+//
+//
+    //Casting
+//        double a;
+//        int b ;
+//        a = 5.0;
+//        b = (int) a;
+//
+//        System.out.println(b);
+
+}
+
+
